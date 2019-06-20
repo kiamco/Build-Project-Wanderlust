a0d7884ef35b4350d425ee4424aed793f409fd08
kiamco <kiamcoo@gmail.com>
2019-06-19 20:58:34 -0700

    fix width

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -4,8 +4,20 @@ class Carousel {
         this.allImg = this.el.querySelectorAll('img');
         this.currentIndex = 0;
         this.allImg[this.currentIndex].style.display = 'block';
+
+
+
+
+
     }
 
+    next() {
+        if (this.currentIndex < 3 || this.currentIndex > -3) {
+            this.allImg.forEach(el => el.style.display = "none");
+            this.currentIndex = (this.currentIndex + 1) % this.allImg.length;
+            this.allImg[this.currentIndex].style.display = 'block';
+        }
+    }
 }
 
 let carousel = document.querySelector('.carousel');
