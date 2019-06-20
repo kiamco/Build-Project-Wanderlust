66ece02f5879f5c5855bb7d36e31eca6677bccc3
kiamco <kiamcoo@gmail.com>
2019-06-20 08:50:47 -0700

    added heading to bg imgs

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -18,19 +18,29 @@ class Carousel {
 
     next() {
         if (this.currentIndex < 3 || this.currentIndex > -3) {
+            // removes heading and goes next
+            this.heading[this.currentIndex].classList.remove('active')
             this.currentIndex = (this.currentIndex + 1) % this.imgArr.length;
+
+            // updates backgournd and heading
             this.el.style.background = `url(${this.imgArr[this.currentIndex]}) center no-repeat`;
             this.el.style.backgroundSize = "cover";
-
+            this.heading[this.currentIndex].classList.toggle('active')
         }
+
+
     }
 
 }
 
 let carousel = document.querySelector('.carousel');
+
+// img array of background picture
 const arr = ["./Images/Horizontal/silas-hao-1072721-unsplash.jpg",
     "./Images/Horizontal/riccardo-chiarini-365677-unsplash.jpg",
     "./Images/Horizontal/timon-klauser-308449-unsplash.jpg",
     "./Images/Horizontal/claudio-hirschberger-1326174-unsplash.jpg"
 ]
+
+// create new carousel
 let createCarousel = new Carousel(carousel, arr);
