c31b7d3587e8d53435549121ff59d319fcd6f123
kiamco <kiamcoo@gmail.com>
2019-06-19 23:54:00 -0700

    completed but may add an auto play functionality

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -1,25 +1,36 @@
 class Carousel {
-    constructor(el) {
+    constructor(el, imgArr) {
         this.el = el;
-        this.allImg = this.el.querySelectorAll('img');
+        this.imgArr = imgArr;
+        this.heading = this.el.querySelectorAll('a');
+        this.left = this.el.querySelector('.fa-angle-left');
+        this.right = this.el.querySelector('.fa-angle-right');
+
         this.currentIndex = 0;
-        this.allImg[this.currentIndex].style.display = 'block';
+        console.log(this.currentIndex);
+        this.el.style.background = `url(${this.imgArr[this.currentIndex]}) center no-repeat`;
+        this.el.style.backgroundSize = "cover";
 
-
-
-
+        this.left.addEventListener('click', this.next.bind(this));
+        this.right.addEventListener('click', this.next.bind(this));
 
     }
 
     next() {
         if (this.currentIndex < 3 || this.currentIndex > -3) {
-            this.allImg.forEach(el => el.style.display = "none");
-            this.currentIndex = (this.currentIndex + 1) % this.allImg.length;
-            this.allImg[this.currentIndex].style.display = 'block';
+            this.currentIndex = (this.currentIndex + 1) % this.imgArr.length;
+            this.el.style.background = `url(${this.imgArr[this.currentIndex]}) center no-repeat`;
+            this.el.style.backgroundSize = "cover";
+
         }
     }
+
 }
 
 let carousel = document.querySelector('.carousel');
-
-let createCarousel = new Carousel(carousel);
+const arr = ["./Images/Horizontal/silas-hao-1072721-unsplash.jpg",
+    "./Images/Horizontal/riccardo-chiarini-365677-unsplash.jpg",
+    "./Images/Horizontal/timon-klauser-308449-unsplash.jpg",
+    "./Images/Horizontal/claudio-hirschberger-1326174-unsplash.jpg"
+]
+let createCarousel = new Carousel(carousel, arr);
