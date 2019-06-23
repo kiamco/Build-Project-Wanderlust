6bdd7a5047dda77a15c4a24be410aa76cf0659ff
elisabethearley <elisabethearley@gmail.com>
2019-06-23 06:21:43 -0400

    added responsiveness for tablet and mobile for home, tour, and guide pages

--- guide.js
+++ guide.js
@@ -1,3 +1,10 @@
 let registerButton = document.querySelector(".registerbtn");
 registerButton.style.cursor = "pointer";
 
+registerButton.addEventListener("mouseover", function( event ) {   
+    event.target.style.background = "#659DBD";
+  
+    setTimeout(function() {
+      event.target.style.background = "";
+    }, 800);
+  }, false);
