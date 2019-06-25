d0591f561fcfe9dcc9b64c944551ffe4dac952fb
elisabethearley <elisabethearley@gmail.com>
2019-06-24 22:04:44 -0400

    added some javascript hovering to images on index.html and guide.html, added extended menu to login button for mobile access

--- guide.js
+++ guide.js
@@ -8,3 +8,20 @@ registerButton.addEventListener("mouseover", function( event ) {
       event.target.style.background = "";
     }, 800);
   }, false);
+
+let guideImages = document.querySelectorAll(".guide-img");
+guideImages.forEach(function (images) {
+    images.addEventListener("mouseover", function(event) {
+    event.target.style.opacity = "0.5";
+
+    setTimeout(function() {
+        event.target.style.opacity = '';
+    }, 500);
+    }, false);
+})  
+
+let guideText = document.querySelectorAll(".guide-img");
+guideText[0].addEventListener("mouseover", function(event) {
+    guideText[0].textContent = "Artsy Fun";
+});
+
