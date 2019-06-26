ffab704f52631c429c079895b4d28340a652ebb1
elisabethearley <elisabethearley@gmail.com>
2019-06-25 23:21:29 -0400

    added text to homepage images for mobile access to other pages

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let destImages = document.querySelectorAll(".dest-img");
 destImages.forEach(function (images) {
     images.addEventListener("mouseover", function(event) {
-    event.target.style.opacity = "0.5";
+    event.target.style.opacity = "0.4";
     setTimeout(function() {
         event.target.style.opacity = '';
-    }, 500);
+    }, 700);
 }, false);
 })
 
