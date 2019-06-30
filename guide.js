153c100724f131f51e4471c1699619ea6a698719
elisabethearley <elisabethearley@gmail.com>
2019-06-30 14:09:53 -0400

    "added text and js to guide page"

--- guide.js
+++ guide.js
@@ -11,17 +11,20 @@ registerButton.addEventListener("mouseover", function( event ) {
 
 let guideImages = document.querySelectorAll(".guide-img");
 guideImages.forEach(function (images) {
-    images.addEventListener("mouseover", function(event) {
-    event.target.style.opacity = "0.5";
-
-    setTimeout(function() {
+    images.addEventListener("click", function(event) {
+      event.target.style.opacity = "0.5";
+    
+    images.addEventListener("click", function(event) {
+      event.target.style.opacity = "";
+    })
+  /*  setTimeout(function() {
         event.target.style.opacity = '';
     }, 500);
-    }, false);
-})  
+    }, false);*/
+})})
 
-let guideText = document.querySelectorAll(".guide-img");
-guideText[0].addEventListener("mouseover", function(event) {
-    guideText[0].textContent = "Artsy Fun";
-});
+
+/*document.getElementById("guide-img-1").addEventListener("click", function(event) {
+    event.innerHTML = "Artsy Fun";
+});*/
 
